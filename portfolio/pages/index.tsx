4f2305bb233b3326519fb8bfe24e516a21eec89c
Varun Gupta <varun.gupta@primathon.in>
2022-09-25 13:48:40 +0530

    Added Dependencies

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-   <>
-   <h1>Hello</h1>
-   </>
+    <>
+      <h1>Hello World</h1>
+    </>
   )
 }
 
