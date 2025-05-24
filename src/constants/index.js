8bb93a91a53953bb15721c45f3381a9b271ba888
Varun Gupta <65545724+varungupta2000@users.noreply.github.com>
2025-05-24 23:05:31 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,14 +63,6 @@ const services = [
 
 const technologies = [
   {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
     name: "JavaScript",
     icon: javascript,
   },
